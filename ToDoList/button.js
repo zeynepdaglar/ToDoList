2b8640ec71875b1fefc41df92d2f10b3cc514359
zeynepdaglar <86071617+zeynepdaglar@users.noreply.github.com>
2022-02-22 19:56:25 +0300

    Update button.js

--- ToDoList/button.js
+++ ToDoList/button.js
@@ -1,11 +1,14 @@
 let icon = document.querySelector('#icon-check')
-icon.addEventListener("click", btnCheck);
+let text = document.querySelector('#li-text').value
+icon.addEventListener("click", btnCheck)
 let counter=1
 
 function btnCheck(){
     counter++
     if(counter%2==0){
           icon.className = "fa-regular fa-circle-check btn-icon"
+          //tıklanması durumunda ustunun cizilmesini sağlar
+          //text.style.textDecoration = "line-through"
     }
     else{
         icon.className = "fa-regular fa-circle"
@@ -14,21 +17,19 @@ function btnCheck(){
 }
 
 //text-box id sine sahip olan elementin içindeki değerleri al
-let textBoxValue = document.querySelector('#text-box').value
+let textBoxValue = document.querySelector('#textbox').value
 //add butonunu sec
 let btnAdd = document.querySelector('#button-addon2')
-//yeni bir li elementi oluştur
-let Li_DOM = document.createElement('li')
 //add butonuna tıklandıgında addList fonksiyonunu çalıştır
-btnAdd.addEventListener("click", addList);
+btnAdd.addEventListener('click', addList)
 
-function addList(){
-    //texBoxValue boş veya null değilse
-    if(textBoxValue != "" && textBoxValue != null){
-         Li_DOM.innerHTML = textBoxValue
+window.onload=function addList(){
+    if(textBoxValue != ""){
+        //yeni bir li elementi oluştur
+        let Li_DOM = document.createElement('li')
+        //yeni li ögesini divLi idsine sahip div içine koy
+        document.querySelector("#divLi").append(Li_DOM)
+        Li_DOM.innerHTML = textBoxValue
+        console.log(textBoxValue)
     }
-    else{
-     //toast
-    }
-
 }
